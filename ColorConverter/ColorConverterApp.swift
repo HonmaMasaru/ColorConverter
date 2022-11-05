123077daa2488d8f09f9f5eb490c85a08d4da756
Honma Masaru <46467282+HonmaMasaru@users.noreply.github.com>
2022-11-05 14:36:41 +0900

    Initial Commit

---
+++ ColorConverter/ColorConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorConverterApp.swift
+//  ColorConverter
+//
+//  Created by Honma Masaru on 2022/11/05.
+//
+
+import SwiftUI
+
+@main
+struct ColorConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
